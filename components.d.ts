8c96deefe858be8ce50150d7656ac4338638a0ed
sugar <engineerzjl@foxmail.com>
2023-06-02 21:23:13 +0800

    chore: 自动导入更新

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    'HelloWorld copy': typeof import('./src/components/HelloWorld copy.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
     IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
